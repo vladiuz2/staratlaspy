aacf87fbbf880d40577465cfd0859cab3f16ec32
vladiuz2 <vlad@staratlas.club>
2022-08-08 13:57:52 +0400

    0.1.31 updated readme to match changes in code

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="staratlaspy",
-    version="0.1.30",
+    version="0.1.31",
     description="Python module for parsing staratlas instructions (based on anchorpy).",
     long_description = long_description,
     long_description_content_type = 'text/markdown',
