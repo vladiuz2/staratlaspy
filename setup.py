ccfdf46e20c42f517c1cf4099ab1f193724db0e6
lukas <git@lukaselsner.de>
2022-09-20 16:40:25 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup, find_packages
-import os, pathlib
+import pathlib
+
+from setuptools import setup
 from setuptools.command.egg_info import egg_info
+
 
 class egg_info_ex(egg_info):
     """Includes license file into `.egg-info` folder."""
@@ -14,6 +16,7 @@ class egg_info_ex(egg_info):
 
         egg_info.run(self)
 
+
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
@@ -23,31 +26,31 @@ setup(
     name="staratlaspy",
     version="0.1.36",
     description="Python module for parsing staratlas instructions (based on anchorpy).",
-    long_description = long_description,
-    long_description_content_type = 'text/markdown',
-    readme = "README.md",
-    license_files = ('LICENSE.txt',),
-    license = 'Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International Public License',
-    cmdclass = {'egg_info': egg_info_ex},
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    readme="README.md",
+    license_files=('LICENSE.txt',),
+    license='Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International Public License',
+    cmdclass={'egg_info': egg_info_ex},
 
-    #url = "https://git.kryptonhub.com/the-club/staratlaspy",
-    author = "Vlad Smirnov",
-    author_email = "vlad@theclubguild.com",
+    # url = "https://git.kryptonhub.com/the-club/staratlaspy",
+    author="Vlad Smirnov",
+    author_email="vlad@theclubguild.com",
     classifiers=[
-      "Development Status :: 3 - Alpha",
-      "Intended Audience :: Developers",
-      "Topic :: Software Development :: Libraries :: Python Modules",
-      "Programming Language :: Python :: 3"
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3"
     ],
-    packages = ['staratlas'] +
-               ['staratlas.utils'] +
-               ['staratlas.marketplace'] +
-               ['staratlas.marketplace.'+ s for s in ['accounts','errors','instructions','types']] +
-               ['staratlas.faction'] +
-               ['staratlas.faction.' + s for s in ['accounts', 'errors', 'instructions']] +
-               ['staratlas.score'] +
-               ['staratlas.score.' + s for s in ['accounts', 'errors', 'instructions', 'types']],
-    package_dir = {'staratlas': '.'},
+    packages=['staratlas'] +
+             ['staratlas.utils'] +
+             ['staratlas.marketplace'] +
+             ['staratlas.marketplace.' + s for s in ['accounts', 'errors', 'instructions', 'types']] +
+             ['staratlas.faction'] +
+             ['staratlas.faction.' + s for s in ['accounts', 'errors', 'instructions']] +
+             ['staratlas.score'] +
+             ['staratlas.score.' + s for s in ['accounts', 'errors', 'instructions', 'types']],
+    package_dir={'staratlas': '.'},
     python_requires=">=3.9, <4",
     install_requires=[
         'construct==2.10.67',
@@ -57,7 +60,7 @@ setup(
         'click==8.1.3',
         'anchorpy[cli]==0.9.1',
         'asyncclick==8.1.3.2',
-        'prettytable=3.4.1'
+        'prettytable==3.4.1'
     ],
     data_files=[("idls", [
         "idls/staratlas-score.json",
@@ -65,11 +68,10 @@ setup(
         "idls/staratlas-distributor.json",
         "idls/staratlas-marketplace.json"
     ])],
-    include_package_data = True,
+    include_package_data=True,
     entry_points={  # Optional
         "console_scripts": [
             "staratlas=staratlas.cli:cli",
         ],
     },
 )
-
